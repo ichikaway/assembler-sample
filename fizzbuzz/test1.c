9690f8964b3571c310cc314e3b1cc4b1e232a097
Yasushi Ichikawa <ichikaway@gmail.com>
2020-05-24 17:51:11 +0900

    sample test

---
+++ fizzbuzz/test1.c
@@ -0,0 +1,7 @@
+#include <stdlib.h>
+
+int main() {
+    int i = 10;
+    int a = i;
+    exit(0);
+}
---
+++ fizzbuzz/test1.s
@@ -0,0 +1,19 @@
+	.file	"test1.c"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB5:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	$0, %edi
+	call	exit@PLT
+	.cfi_endproc
+.LFE5:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 7.4.0-1ubuntu1~18.04.1) 7.4.0"
+	.section	.note.GNU-stack,"",@progbits
