6483363e24bad0861b3e74f5767528ae47242550
Yasushi Ichikawa <ichikaway@gmail.com>
2020-06-07 18:06:34 +0900

    function call output

--- fizzbuzz/test1.c
+++ fizzbuzz/test1.c
@@ -1,7 +1,12 @@
 #include <stdlib.h>
 
+int hoge() {
+    return 11;
+}
+
 int main() {
     int i = 10;
     int a = i;
+    hoge();
     exit(0);
 }
--- fizzbuzz/test1.s
+++ fizzbuzz/test1.s
@@ -1,19 +1,43 @@
 	.file	"test1.c"
+	.intel_syntax noprefix
 	.text
+	.globl	hoge
+	.type	hoge, @function
+hoge:
+.LFB5:
+	.cfi_startproc
+	push	rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	mov	rbp, rsp
+	.cfi_def_cfa_register 6
+	mov	eax, 11
+	pop	rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE5:
+	.size	hoge, .-hoge
 	.globl	main
 	.type	main, @function
 main:
-.LFB5:
+.LFB6:
 	.cfi_startproc
-	pushq	%rbp
+	push	rbp
 	.cfi_def_cfa_offset 16
 	.cfi_offset 6, -16
-	movq	%rsp, %rbp
+	mov	rbp, rsp
 	.cfi_def_cfa_register 6
-	movl	$0, %edi
+	sub	rsp, 16
+	mov	DWORD PTR -8[rbp], 10
+	mov	eax, DWORD PTR -8[rbp]
+	mov	DWORD PTR -4[rbp], eax
+	mov	eax, 0
+	call	hoge
+	mov	edi, 0
 	call	exit@PLT
 	.cfi_endproc
-.LFE5:
+.LFE6:
 	.size	main, .-main
 	.ident	"GCC: (Ubuntu 7.4.0-1ubuntu1~18.04.1) 7.4.0"
 	.section	.note.GNU-stack,"",@progbits
