ab5a400a1eaab432f9020c707cdaff220a0a2225
Yasushi Ichikawa <ichikaway@gmail.com>
2020-06-08 22:18:28 +0900

    整数をasciiの数字文字に変換してスタックに積む

--- fizzbuzz/fizzbuzz.s
+++ fizzbuzz/fizzbuzz.s
@@ -65,18 +65,37 @@ buzz:
     .ascii "buzz"
 newline:
     .ascii "\n"
+nullbyte:
+    .ascii "\0"
 
 echocount:
     push rbp
     mov rbp, rsp
 
-    #mov rdi, 0x01#文字数
-    #mov rsi, rsp
-    #sub rsi, 0x70 #rsp-0x70のアドレスをrsiにセット
-    #mov [rsi], r14 #rsiのアドレスの参照先にカウンタr14の値をセット
-    #call put
+    mov r13, 0 #数字の桁数
+    push [nullbyte]
+    mov r11, rcx
 
-    mov rdi, 0x02 #文字数
+echocount_div_again:
+    # カウンタの数字の下位の桁から数字文字列変換してスタックに積む
+    inc r13
+    mov rdx,0 #割られる上位
+    mov rax, r11 #割られる下位
+    mov r12, 10 #割る数
+    div r12
+    mov r11, rax #商 rax
+    or rdx, 0x30 #余りrdxの数字をascii文字の数字に変換
+    push rdx #余りrdx
+    cmp rax, 0 #余りがrdx, 商 rax
+    # 商がゼロでなければまだ桁が残っているためループする
+    jne echocount_div_again
+
+    # nullbyteがくるまでpopして数字を文字列に変換してstr変数のアドレスにいれていく
+    pop r12
+    #cmp r12, 0
+
+    mov rdi, r13 #文字数
+    mov [str], r12
     lea rsi, [str]
     call put
 
